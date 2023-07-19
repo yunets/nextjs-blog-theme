53028922d14cae875c2120d40fc761f8b64d2d97
yunets <724547564@qq.com>
2023-07-19 11:17:00 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,10 +1,10 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : '免费的chatgpt网站';
+    : '公益导航网站-chatgpt4.0';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : '免费的chatgpt网站';
+    : '公益导航网站-chatgpt4.0';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
     : 'All rights reserved.-http://124.220.104.235:31105/web/chatgpt';
